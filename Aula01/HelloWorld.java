e30ba90b7b6407014a04a73b749220bbcfad405a
katherinethyphanny-jpg <katherinethyphanny@gmail.com>
2026-08-03 23:08:32 -0300

    Aula 01 exemplo java

---
+++ Aula01/HelloWorld.java
@@ -0,0 +1,12 @@
+package Aula01;
+
+// Classe chamada HelloWorld
+public class HelloWorld {
+
+    // Cria uma função
+    public static void main(String[] args) {
+        // Imprime a mensagem "Hello World!" no console
+        System.out.println("Hello World!");
+    }
+    
+}
